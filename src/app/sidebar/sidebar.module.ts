dd6167c6f670df70f5f60f8888149a38b05ca971
Mauro Rossi <maurorossi@maurorossi.com>
2025-05-08 17:38:34 -0300

    removid referencias a sidebar

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { SidebarComponent } from './sidebar.component';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule ],
-    declarations: [ SidebarComponent ],
-    exports: [ SidebarComponent ]
+    imports: [ RouterModule, CommonModule ]
 })
 
 export class SidebarModule {}
