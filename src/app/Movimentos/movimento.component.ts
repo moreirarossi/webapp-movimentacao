9485ef91da047eb6df1dfcbe9a78cc74e06329e9
Mauro Rossi <maurorossi@maurorossi.com>
2025-05-08 15:24:58 -0300

    tela e botões concluidos

--- src/app/Movimentos/movimento.component.ts
+++ src/app/Movimentos/movimento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovimentoService } from 'app/services/movimento.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-movimento',
@@ -18,11 +19,53 @@ export class MovimentoComponent implements OnInit {
   ];
   movimentos: any[] = [];
 
-  constructor(private movimentoService: MovimentoService) {}
-
+  constructor(
+    private movimentoService: MovimentoService,
+    private fb: FormBuilder
+  ) {}
+  
   ngOnInit(): void {
     this.movimentoService.buscarListaMovimentos().subscribe((data: any[]) => {
       this.movimentos = data;
+      this.movimentoForm.disable();
     });
   }
+
+  movimentoForm = this.fb.group({
+    datMes: [null, [Validators.required, Validators.pattern('^[0-9]{1,2}$')]],
+    datAno: [null, [Validators.required, Validators.pattern('^[0-9]{4}$')]],
+    codProduto: [null, Validators.required],
+    codCosif: [null, Validators.required],
+    valValor: [null, [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+    desDescricao: [null, [Validators.required, Validators.maxLength(200)]],
+  });
+
+  produtos = [
+    { codigo: 'P001', descricao: 'Produto 1' },
+    { codigo: 'P002', descricao: 'Produto 2' }
+  ];
+  
+  cosifs = [
+    { codigo: 'C001', descricao: 'Cosif 1' },
+    { codigo: 'C002', descricao: 'Cosif 2' }
+  ];
+  
+  adicionarMovimento() {
+    if (this.movimentoForm.invalid) return;
+  
+    const novoMovimento = this.movimentoForm.value;
+    this.movimentos.push(novoMovimento);
+    this.movimentos = [...this.movimentos];
+    this.movimentoForm.reset();
+  }
+
+  novo() {
+    this.movimentoForm.enable();
+  }
+  
+  limpar() {
+    this.movimentoForm.reset();
+    this.movimentoForm.disable();
+  }
 }
+
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { LbdModule } from '../../lbd/lbd.module';
-
 import { AdminLayoutRoutes } from './admin-layout.routing';
-
 import { HomeComponent } from '../../home/home.component';
 import { ClienteComponent } from 'app/Clientes/cliente-listar/cliente.component';
 import { MovimentoComponent } from 'app/Movimentos/movimento.component';
